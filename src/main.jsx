63a29285758410ace517074789b389eb89b98d25
Zaman <zamanofficial1980@gmail.com>
2024-04-18 00:03:48 +0530

    committed some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider,  createBrowserRouter, Route, Routes, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout'
 import About from './components/About/About'
 import Home from './components/Home/Home'
 import ProSkill from './components/ProSkill/ProSkill'
 import Edu from './components/Education/Edu'
 
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element :<Layout/>,
-    children : [
-      {
-        index : 'Home',
-        element : <Home/>
-      },
-      {
-        path : "about",
-        element : <About />
-      },
-      {
-        path : "ProSkill",
-        element : <ProSkill />
-      },
-      {
-        path : "Edu",
-        element : <Edu />
-      }
+
+//  First Method of Rendering coomponents
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path ="/" element={<Layout/>}>
+      <Route index element= {<Home/>}/>
+      <Route path='About' element= {<About/>}/>
+      <Route path='ProSkill' element= {<ProSkill/>}/>
+      <Route path='Edu' element= {<Edu/>}/>
+    </Route>
+  )
+)
+
+//----Second Method of rendering components-----------
+
+// const router = createBrowserRouter([
+//   {
+//     path : "/",
+//     element :<Layout/>,
+//     children : [
+//       {
+//         index : 'Home',
+//         element : <Home/>
+//       },
+//       {
+//         path : "about",
+//         element : <About />
+//       },
+//       {
+//         path : "ProSkill",
+//         element : <ProSkill />
+//       },
+//       {
+//         path : "Edu",
+//         element : <Edu />
+//       }
       
-    ]
-  }
-])
+//     ]
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
